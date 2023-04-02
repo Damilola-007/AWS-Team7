1435877160cc8b53b71ae87e4bc6b44c6f4b857f
Thant-Thu-Aung <116348648+Thant-Thu-Aung@users.noreply.github.com>
2023-04-02 02:33:40 +0100

    Delete Investment(Need Xampp) directory

--- Investment(Need Xampp)/Logout.php
+++
@@ -1,9 +0,0 @@
-<?php  
-
-session_start();
-require('connect.php');
-echo "<script> window.alert('Logged out Successfully')</script>";
-echo "<script> window.location.assign('Login.php')</script>";
-session_destroy();
-
-?>
