98a49a5e180df75035ac8d35f74a54008c16e7fb
Thant-Thu-Aung <116348648+Thant-Thu-Aung@users.noreply.github.com>
2023-04-03 08:07:19 +0100

    Added checks for JavaScript enabled or not.
    I have also fixed the read more and show less not working properly which is in previous versions, clicking read more would show all the abstract and content in all rows and show less would hide all the abstract and content in all rows. But in this fix, it would show or hide the specific abstract or content based on "Read more" or "Show less" user has clicked on which specific column.

---
+++ Investment(Need Xampp)/js/Readmore_Showless.js
@@ -0,0 +1,32 @@
+function readmoreA(id)
+{
+  document.getElementById('show-cutA_'+id).style.display='none';
+  document.getElementById('read-moreA_'+id).style.display='none';
+  document.getElementById('show-contentA_'+id).style.display='block';
+  document.getElementById('show-lessA_'+id).style.display='block';
+}
+
+function showlessA(id)
+{
+  document.getElementById('show-cutA_'+id).style.display='block';
+  document.getElementById('read-moreA_'+id).style.display='block';
+  document.getElementById('show-contentA_'+id).style.display='none';
+  document.getElementById('show-lessA_'+id).style.display='none';
+}
+
+function readmoreC(id)
+{
+  document.getElementById('show-cutC_'+id).style.display='none';
+  document.getElementById('read-moreC_'+id).style.display='none';
+  document.getElementById('show-contentC_'+id).style.display='block';
+  document.getElementById('show-lessC_'+id).style.display='block';
+}
+
+function showlessC(id)
+{
+  document.getElementById('show-cutC_'+id).style.display='block';
+  document.getElementById('read-moreC_'+id).style.display='block';
+  document.getElementById('show-contentC_'+id).style.display='none';
+  document.getElementById('show-lessC_'+id).style.display='none';
+}
+
