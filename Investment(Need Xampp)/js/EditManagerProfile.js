fa5021f9d791f9638b2ce3f8ca06744f5144cca1
Thant-Thu-Aung <116348648+Thant-Thu-Aung@users.noreply.github.com>
2023-04-04 18:20:49 +0100

    Update EditManagerProfile.js

--- Investment(Need Xampp)/js/EditManagerProfile.js
+++ Investment(Need Xampp)/js/EditManagerProfile.js
@@ -19,4 +19,12 @@ function editmanageroptions()
   document.getElementById('txtchangedpassword').style.display='block';
   document.getElementById('editbutton').style.display='block';
   document.getElementById('deletebutton').style.display='block';
+
+  function Change()
+  {
+   var pass = document.getElementById("passvariable");
+            pass.value = "2";
+  }
+
+  document.getElementById('txtchangedpassword').onchange = Change;
 }
